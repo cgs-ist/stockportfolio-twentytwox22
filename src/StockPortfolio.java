11a76c222d46717dd34f903862e2f48ce248ac98
junrui <49829931+twentytwox22@users.noreply.github.com>
2023-07-25 11:59:34 +1000

    final
    not too sure if this works -- copy pasted code from my other repo

---
+++ src/StockPortfolio.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockPortfolio {
+    static HashMap<Stock, Integer> portfolio = new HashMap<>();
+
+    public static void viewPortfolio() {
+        System.out.println("Portfolio Value:" + totalValue() +"\n");
+        for (Stock stock : portfolio.keySet()) {
+            System.out.println("stock name: " + stock.getName() + " ["+ stock.getTicker() +"]" + "\n" + "stocks owned: " + portfolio.get(stock)
+                    + "\n" + "stock value: " + stock.getPrice() * portfolio.get(stock) +"\n");
+        }
+    }
+
+    public static double totalValue() {
+        double value = 0;
+        for (Stock stock : portfolio.keySet()) {
+            value +=  stock.getPrice() * portfolio.get(stock);
+        }
+        return value;
+    }
+
+    public void put(Stock stock, int i) {
+        portfolio.put(stock, i);
+    }
+}
